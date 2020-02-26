2e1163ad71341d554611c496dac2ea9de201c4ab
ethLengfeld <elengfeld@wisc.edu>
2020-02-25 19:48:13 -0600

    added class headers

---
+++ SummarizeGrades.cpp
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File: SummarizeGrades.cpp, demo.cpp
+// This File: SummarizeGrades.cpp
+// Other Files: 	 SummarizeGrades.h / SummarizeGrades.cpp / demo.cpp / Makefile / README.md
+// Semester:         CS 368 Spring 2020
+//
+// Author:           Ethan Lengfeld
+// Email:            elengfeld@wisc.edu
+// CS Login:         lengfeld
+//
+//
+// Online sources:   
+//
+////////////////////////////////////////////////////////////////////////////////
+
+/**
+ * This class will implement the functions defined in the 
+ * SummarizeGrades.h. 
+*/
+#include "SummarizeGrades.h"
---
+++ SummarizeGrades.h
@@ -0,0 +1,14 @@
+////////////////////////////////////////////////////////////////////////////////
+// Main File: SummarizeGrades.cpp, demo.cpp
+// This File: SummarizeGrades.h
+// Other Files: 	 SummarizeGrades.h / SummarizeGrades.cpp / demo.cpp / Makefile / README.md
+// Semester:         CS 368 Spring 2020
+//
+// Author:           Ethan Lengfeld
+// Email:            elengfeld@wisc.edu
+// CS Login:         lengfeld
+//
+//
+// Online sources:   
+//
+////////////////////////////////////////////////////////////////////////////////
